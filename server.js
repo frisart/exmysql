3ec29d9dedd6633cd1405df5c0bf06c29101b1d2
unknown <gerylinux@gmail.com>
2020-07-07 11:21:24 +0700

    buat index point file server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+let whitelis = [
+    'http://localhost:8081'
+];
+
+let corsOption = {
+    origin: function(origin, callback){
+        if (whitelis.indexOf(origin) !== 1|| !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not Allow by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOption));
+
+// parse request application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
+
+app.get('/', (req, res) => {
+     res.json({
+         message: "Welcome to ExMysql"
+     });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
